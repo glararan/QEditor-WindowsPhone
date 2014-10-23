95d5f6026a2da0c05aa62fea5be619b0923ae207
fasdavid <davidpodzimek1@gmail.com>
2014-10-23 16:40:42 +0200

    [1] Initial Commit

---
+++ Tracking/DataWork.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace Tracking
+{
+    public class DataWork
+    {
+        public struct PointA
+        {
+            public double X, Y, Z;
+
+            public PointA(double x, double y, double z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+
+        }
+        private double getBigX(List<PointA> track)
+        {
+            double nej;
+            if (track[0].X > track[1].X)
+            {
+                nej = track[0].X;
+            }
+            else
+            {
+                nej = track[1].X;
+            }
+            for (int i = 2; i < track.Count; i++)
+            {
+                if (nej < track[i].X)
+                {
+                    nej = track[i].X;
+                }
+            }
+            return nej;
+        }
+
+        private double getLittleX(List<PointA> track)
+        {
+            double nej;
+            if (track[0].X > track[1].X)
+            {
+                nej = track[1].X;
+            }
+            else
+            {
+                nej = track[0].X;
+            }
+            for (int i = 2; i < track.Count; i++)
+            {
+                if (nej > track[i].X)
+                {
+                    nej = track[i].X;
+                }
+            }
+            return nej;
+        }
+
+        private double getBigY(List<PointA> track)
+        {
+            double nej;
+            if (track[0].X > track[1].X)
+            {
+                nej = track[0].X;
+            }
+            else
+            {
+                nej = track[1].X;
+            }
+            for (int i = 2; i < track.Count; i++)
+            {
+                if (nej < track[i].X)
+                {
+                    nej = track[i].X;
+                }
+            }
+            return nej;
+        }
+
+        private double getLittleY(List<PointA> track)
+        {
+            double nej;
+            if (track[0].X > track[1].X)
+            {
+                nej = track[1].X;
+            }
+            else
+            {
+                nej = track[0].X;
+            }
+            for (int i = 2; i < track.Count; i++)
+            {
+                if (nej > track[i].X)
+                {
+                    nej = track[i].X;
+                }
+            }
+            return nej;
+        }
+
+        public void dataWork(List<PointA> track)
+        {
+            double x0 = getLittleX(track);
+            double x1 = getBigX(track);
+            double y0 = getLittleY(track);
+            double y1 = getBigY(track);
+
+            Point point1=new Point();
+            Point point2 = new Point();
+            Point point3 = new Point();
+            Point point4 = new Point();
+            //Ohraničení oblasti...
+            point1.X = x1;
+            point1.Y = y0;
+
+            point2.X = x0;
+            point2.Y = y0;
+
+            point3.X = x1;
+            point3.Y = y1;
+
+            point4.X = x0;
+            point4.Y = y1;
+
+        }
+    }
+}
---
+++ Tracking/MainPage.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Xml.Linq;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+using Windows.UI.Xaml.Resources;
+
+namespace Tracking
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public bool start = false;
+        public bool tracking = false;
+        Geolocator geolocator = null;
+        List<DataWork.PointA> track = new List<DataWork.PointA>();
+
+        public MainPage()
+        {
+
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+
+            if (!tracking)
+            {
+                geolocator = new Geolocator();
+                geolocator.DesiredAccuracy = PositionAccuracy.High;
+                geolocator.MovementThreshold = 100;
+
+                geolocator.StatusChanged += geolocator_StatusChanged;
+                geolocator.PositionChanged += geolocator_PositionChanged;
+
+                tracking = true;
+                procT.Text = "Zaznamenávání dat..";
+            }
+            else
+            {
+                geolocator.PositionChanged -= geolocator_PositionChanged;
+                geolocator.StatusChanged -= geolocator_StatusChanged;
+                geolocator = null;
+
+                tracking = false;
+                procT.Text = "Vypnuto";
+            }
+        }
+
+        async void geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            DataWork.PointA pointA = new DataWork.PointA();
+            pointA.X = double.Parse(args.Position.Coordinate.Latitude.ToString());
+            pointA.Y = double.Parse(args.Position.Coordinate.Longitude.ToString());
+            pointA.Z = double.Parse(args.Position.Coordinate.Altitude.ToString());
+            track.Add(pointA);
+        }
+
+       async void geolocator_StatusChanged(Geolocator sender, StatusChangedEventArgs args)
+        {
+
+            switch (args.Status)
+            {
+                case PositionStatus.Disabled:
+                    statusT1.Text = "Povolte sledování v nastavení.";
+                    break;
+                case PositionStatus.Initializing:
+                    statusT1.Text = "Čekání na data...";
+                    break;
+                case PositionStatus.NoData:
+                    statusT1.Text = "Data nejsou k dispozici.";
+                    break;
+                case PositionStatus.Ready:
+                    statusT1.Text = "Data k dispozici...";
+                    break;
+                case PositionStatus.NotAvailable:
+                    statusT1.Text = "Připojení není dispozici";
+                    break;
+                case PositionStatus.NotInitialized:
+                    statusT1.Text = "Nepodařilo se získat připojení";
+                    break;
+            }
+        }
+
+    }
+}
